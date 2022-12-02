ceb901825627be3868664677f7c1288151436dad
Yulia <iqeen94@gmail.com>
2022-12-02 23:33:52 +0200

    Bluetooth try

--- HomeLight.ino
+++ HomeLight.ino
@@ -1,18 +1,34 @@
 #include "DisplayWorker.h"
 #include "WiFiWorker.h"
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
 short time_counter = 10;
 DisplayWorker displayWorker;
 WiFiWorker wifiWorker;
 
+BluetoothSerial SerialBT;
+
 void setup() {
   Serial.begin(9600);
+
+  SerialBT.begin("HomeLight");  //Bluetooth device name
 
   wifiWorker.init();
   displayWorker.initDisplay();
 }
 
 void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+
   displayWorker.write(wifiWorker.doJob());
 
   delay(1000);
